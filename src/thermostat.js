f439f8ce8adf17c6131f6d984b4a4c77483b0b8c
Joe Ball <joe.ball@bglgroup.co.uk>
2019-11-13 11:47:41 +0000

    power saving mode logic added and test passed

--- src/thermostat.js
+++ src/thermostat.js
@@ -4,11 +4,17 @@ class Thermostat {
     constructor() {
         this.temperature = 20;
         this._MINTEMP = 10;
+        this.powerSavingMode = true;
+        this._SAVINGMAXTEMP = 25;
     }
 };
 
 Thermostat.prototype.increase = function (number) {
-    this.temperature += number
+    if (this.powerSavingMode = true && this.temperature + number > this._SAVINGMAXTEMP) {
+        this.temperature = this._SAVINGMAXTEMP;
+    } else {
+        this.temperature += number
+    }
 };
 
 Thermostat.prototype.decrease = function (number) {
