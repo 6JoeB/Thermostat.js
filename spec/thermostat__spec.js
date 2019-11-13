0bab03bdfdf3433d4c43ad244fb0b64fa958638d
Joe Ball <joe.ball@bglgroup.co.uk>
2019-11-13 11:58:02 +0000

    created spec for turning power saving mode on and off

--- spec/thermostat__spec.js
+++ spec/thermostat__spec.js
@@ -13,19 +13,19 @@ describe("Thermostat", function () {
     });
 
     it('starts with a temperature of 20', function () {
-        expect(thermostat.temperature).toEqual(20)
+        expect(thermostat.temperature).toEqual(20);
     });
 
     describe("Decreasing temp", function () {
 
         it('can decrease the temperature with a decrease function', function () {
             thermostat.decrease(1);
-            expect(thermostat.temperature).toEqual(19)
+            expect(thermostat.temperature).toEqual(19);
         });
 
         it('has a minimum temperature of 10', function () {
-            thermostat.decrease(15)
-            expect(thermostat.temperature).toEqual(10)
+            thermostat.decrease(15);
+            expect(thermostat.temperature).toEqual(10);
         });
 
     });
@@ -34,16 +34,22 @@ describe("Thermostat", function () {
 
         it('can increase the temperature with an increase function', function () {
             thermostat.increase(1);
-            expect(thermostat.temperature).toEqual(21)
+            expect(thermostat.temperature).toEqual(21);
         });
 
         it('has a power saving mode that defaults to on', function () {
-            expect(thermostat.powerSavingMode).toEqual(true)
+            expect(thermostat.powerSavingMode).toEqual(true);
         });
 
         it('has a power saving mode that sets the max temp to 25', function () {
-            thermostat.increase(100)
-            expect(thermostat.temperature).toEqual(25)
+            thermostat.increase(100);
+            expect(thermostat.temperature).toEqual(25);
+        });
+
+        it('has a max temp of 32 when power saving mode is off', function () {
+            thermostat.powerSavingModeButton(false);
+            thermostat.increase(100);
+            expect(thermostat.temperature).toEqual(32);
         });
 
     });
